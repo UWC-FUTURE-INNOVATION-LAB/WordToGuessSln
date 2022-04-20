740baeec0aaf5bc115cc3a6bd32e833e1796a790
Allan Pead <adpead@gmail.com>
2022-04-20 15:56:52 +0200

    Add project files.

---
+++ WordToGuess/GuessingGame.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordToGuess
+{
+    public class GuessingGame
+    {
+        private string _guessword;
+        private string _guessProgress;
+
+        public GuessingGame(string guessword)
+        {
+            _guessword = guessword;
+
+            for (int index = 0; index < _guessword.Length; index++)
+            {
+                _guessProgress += "*";
+            }
+        }
+
+        public string GetGuessProgress()
+        {
+            return _guessProgress;
+        }
+
+        
+        public void AddGuess(char letter)
+        {
+            char[] guessProgressArray = _guessProgress.ToCharArray();
+
+            for (int index=0;index < _guessword.Length; index++)
+            {
+                if(_guessword[index] == letter)
+                {
+                    guessProgressArray[index] = letter;
+                }
+            }
+
+            _guessProgress = new string(guessProgressArray);
+
+        }
+
+    }
+}
---
+++ WordToGuess/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WordToGuess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GuessingGame game = new GuessingGame("Harry Potter");
+
+            Console.WriteLine(game.GetGuessProgress());
+
+            while (true)
+            {
+                Console.WriteLine("Please enter next guess: ");
+                string guess = Console.ReadLine();
+
+                game.AddGuess(guess[0]);
+
+                Console.WriteLine(game.GetGuessProgress());
+            }
+        }
+    }
+}
